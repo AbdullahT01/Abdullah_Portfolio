bbdf37ff79538917d977f2e696859e9c86c9c0d4
Abdullah <abdullahtahad30@gmail.com>
2024-06-14 15:15:56 -0400

    Created new Files (css and jsx) for the project cards section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'; 
 import Hero from './sections/Hero/Hero';
 import { ThemeProvider } from './common/ThemeContext';
-
+import Projects from './sections/Hero/Projects/Projects';
 function App() {
   
 
   return (
   <ThemeProvider>
     <Hero />
+    <Projects/>
   </ThemeProvider>
   )
 }
---
+++ src/sections/Hero/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import styles from './projectsStyle.module.css';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      HELLOOOOO
+    </section>
+  )
+}
+
+export default Projects
